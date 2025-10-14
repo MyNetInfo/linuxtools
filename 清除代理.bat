c73756618259256acfc2592d3f9729ee87292682
MyNetInfo <sbpfikdm@gmail.com>
2025-10-14 16:53:47 +0800

    代理设置

--- 清除代理.bat
+++ 清除代理.bat
@@ -3,4 +3,6 @@
 git config --unset http.proxy
 git config --unset https.proxy
 
+echo 【%date% %time%】 代理清除完毕 ..
+
 PAUSE
--- 设置代理.bat
+++ 设置代理.bat
@@ -2,4 +2,6 @@
 
 git config http.proxy 'http://127.0.0.1:6601'
 
+echo 【%date% %time%】 代理设置成功 ..
+
 PAUSE
