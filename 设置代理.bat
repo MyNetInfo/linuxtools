78ced8d6f73f28059bddd936355ae9a580dba089
MyNetInfo <sbpfikdm@gmail.com>
2025-10-14 17:01:11 +0800

    ??

--- 设置代理.bat
+++ 设置代理.bat
@@ -1,6 +1,9 @@
 @ECHO OFF & color 0a
 
-git config http.proxy 'http://127.0.0.1:6601'
+git config http.proxy  http://127.0.0.1:6601
+git config https.proxy http://127.0.0.1:6601
+REM git config http.proxy  http://100.66.66.104:6600
+REM git config https.proxy http://100.66.66.104:6600
 
 echo 【%date% %time%】 代理设置成功 .. 11
 
